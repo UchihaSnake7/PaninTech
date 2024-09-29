d013f194fa8be61390cc15cc0087df98b5eec7c4
¨Rickerbys¨ <uchihasnake7@gmail.com>
2024-09-29 14:12:27 -0400

    Primer commit

---
+++ src/main/java/com/panin/PaninTech/App.java
@@ -0,0 +1,13 @@
+package com.panin.PaninTech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
