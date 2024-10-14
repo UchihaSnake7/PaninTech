92d50662bb99e54857b5877addd46120028b73dd
Eliannis <elianniscortez@gmail.com>
2024-10-13 20:17:46 -0400

    Merge Cambios rbermudez y ecortez corregidos

---
+++ src/main/java/com/panin/controladores/ControladorComprasProductos.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.panin.controladores;
+
+import com.panin.HibernateUtil;
+import com.panin.dto.formAgregarInsumoProductoDTO;
+import com.panin.entidades.ComprasInsumo;
+import com.panin.entidades.ComprasProducto;
+import com.panin.entidades.InsumoRecetas;
+import com.panin.entidades.Producto;
+import com.panin.entidades.Recetas;
+import jakarta.persistence.TypedQuery;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author ricke
+ */
+public class ControladorComprasProductos {
+
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    public ControladorComprasProductos() {
+//         session.beginTransaction();
+    }
+
+    public List<ComprasProducto> obtenerComprasInsumos() {
+        session.beginTransaction();
+        List<ComprasProducto> comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findAll");
+        comprasProductos = query.getResultList();
+
+        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+        session.close();
+        return comprasProductos;
+    }
+
+    public List<ComprasProducto> obtenerComprasdeUnInsumo(Producto producto) {
+        session.beginTransaction();
+        List<ComprasProducto> comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findByProducto");
+        query.setParameter("producto", producto);
+        comprasProductos = query.getResultList();
+
+        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+        session.close();
+        return comprasProductos;
+    }
+
+    public void cerrarSesion() {
+        session.close();
+    }
+
+    public ComprasProducto obtenerComprasdeUnInsumoUnico(Producto producto) {
+//        session.beginTransaction();
+        ComprasProducto comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findByProductoUnico");
+        query.setParameter("producto", producto);
+        query.setMaxResults(1);
+        comprasProductos = (ComprasProducto) query.getSingleResult();
+
+//        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+//        session.close();
+        return comprasProductos;
+    }
+
+    public boolean save(ComprasProducto compra) {
+        try {
+            session.beginTransaction();
+            session.save(compra);
+            System.out.println(session.getTransaction().getStatus());
+            session.close();
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public List<ComprasProducto> obtenerComprasDesde(Producto producto, Date date) {
+        session.beginTransaction();
+        List<ComprasProducto> comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findByProductoDesde");
+        query.setParameter("producto", producto);
+        query.setParameter("desde", date);
+        comprasProductos = query.getResultList();
+
+        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+        session.close();
+        return comprasProductos;
+    }
+
+    public List<ComprasProducto> obtenerComprasDesdeHasta(Producto producto, Date date, Date hasta) {
+        session.beginTransaction();
+        List<ComprasProducto> comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findByProductoFechas");
+        query.setParameter("producto", producto);
+        query.setParameter("desde", date);
+        query.setParameter("hasta", hasta);
+        comprasProductos = query.getResultList();
+
+        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+        session.close();
+        return comprasProductos;
+    }
+
+    public List<ComprasProducto> obtenerComprasHasta(Producto producto, Date hasta) {
+        session.beginTransaction();
+        List<ComprasProducto> comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findByProductoHasta");
+        query.setParameter("producto", producto);
+        query.setParameter("hasta", hasta);
+        comprasProductos = query.getResultList();
+
+        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+        session.close();
+        return comprasProductos;
+    }
+
+    public List<ComprasProducto> obtenerComprasInsumosFecha(Date desde, Date hasta) {
+        session.beginTransaction();
+        List<ComprasProducto> comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findByFechas");
+        query.setParameter("desde", desde);
+        query.setParameter("hasta", hasta);
+        comprasProductos = query.getResultList();
+
+        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+        session.close();
+        return comprasProductos;
+    }
+
+    public List<ComprasProducto> obtenerComprasInsumosHasta(Date hasta) {
+        session.beginTransaction();
+        List<ComprasProducto> comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findByHasta");
+
+        query.setParameter("hasta", hasta);
+        comprasProductos = query.getResultList();
+
+        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+        session.close();
+        return comprasProductos;
+
+    }
+
+    public List<ComprasProducto> obtenerComprasInsumosDesde(Date desde) {
+        session.beginTransaction();
+        List<ComprasProducto> comprasProductos;
+
+        TypedQuery query = session.getNamedQuery("ComprasProducto.findByDesde");
+
+        query.setParameter("desde", desde);
+        comprasProductos = query.getResultList();
+
+        session.getTransaction().commit();
+        //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+        session.close();
+        return comprasProductos;
+
+    }
+
+}
--- src/main/java/com/panin/controladores/ControladorProductos.java
+++ src/main/java/com/panin/controladores/ControladorProductos.java
@@ -31,148 +31,25 @@ public class ControladorProductos {
 
     public ControladorProductos() {
 //		 conexionDB = new ConexionDB("jdbc:mysql://localhost:3306/panin","root","root");
-//		session.beginTransaction();
+        session.beginTransaction();
 
     }
 
     //Ejemplo de metodo para obtener todos los productos de la db
     public List<Producto> obtenerProductos() {
-
-        session.beginTransaction();
-
         List<Producto> productos;
 
-<<<<<<< HEAD
-            session.save(producto);
-            
-             session.getTransaction().commit();
-
-            
-        }
-         
-          public void actualizarProducto(Producto producto){
-            
-            session.beginTransaction();
-
-            session.update(producto);
-            
-            session.getTransaction().commit();
-
-            
-        }
-         
-         public void borrarProducto(Producto producto){
-            		
-            session.beginTransaction();
-            
-            session.delete(producto);
-                        
-            session.getTransaction().commit();
-            
-            if(producto.getTipo().toString().equalsIgnoreCase("Elaborado")){
-                
-                 ControladorReceta cr = new ControladorReceta();
-                Recetas r = cr.obtenerRecetaPorId(producto.getIdReceta());
-                cr.borrarReceta(r);
-            }
-            
-        }
-	
-     public BigDecimal calcularPrecioProduccion(Producto producto, double cantidad, DefaultTableModel... modelTable ){
-
-        this.cerrarSesion();
-        List<formAgregarInsumoProductoDTO> listaInsumosReceta = new ArrayList<formAgregarInsumoProductoDTO>();
-        
-        ControladorReceta cr = new ControladorReceta();
-        Recetas r = new Recetas();
-        r = cr.obtenerRecetaPorId(producto.getIdReceta());
-        
-        double cantidadReceta = r.getCantidad();
-        double cantidadCalculada = 0.0;
-        double precioTotal = 0;
-        double precio = 0.0;
-        
-        ControladorComprasInsumos cpi = new ControladorComprasInsumos();
-         
-        if(cantidadReceta > cantidad){
-            cantidadCalculada =  cantidad / cantidadReceta;
-        }
-        else if(cantidadReceta < cantidad){
-            cantidadCalculada = cantidad / cantidadReceta ;
-
-        }
-        else if(cantidadReceta == cantidad){
-            cantidadCalculada = 1;
-        }
-        
-//         List<InsumoRecetas> lir =  (List<InsumoRecetas>) r.getInsumoRecetasCollection();
-
-           cr.abrirSesion();
-           
-           if(modelTable.length > 0 ){
-              modelTable[0].setRowCount(0);
-
-           }
-
-        for(InsumoRecetas ir : r.getInsumoRecetasCollection()) {
-                    	
-              formAgregarInsumoProductoDTO dto = new formAgregarInsumoProductoDTO();
-                    	
-               dto.setCantidad(ir.getCantidad().doubleValue() * cantidadCalculada);
-               dto.setInsumo(ir.getIdInsumo());
-               dto.setUnidadMedidad(ir.getUnidadMedida());
-                    	
-               listaInsumosReceta.add(dto);
-               
-               ComprasInsumo ci = cpi.obtenerComprasdeUnInsumoUnico(ir.getIdInsumo());
-               
-//               
-//               System.out.print("\nci.getPrecio() " + ci.getPrecio().toString());
-//               System.out.print("\nir.getCantidad() " + ir.getCantidad().toString());
-//               System.out.print("\n ci.getPrecio().multiply(ir.getCantidad()) " + ci.getPrecio().multiply(ir.getCantidad()).toString());
-//
-//               System.out.print("\ncantidadCalculada " + cantidadCalculada);
-//               System.out.print("\n ci.getPrecio().multiply(ir.getCantidad()).multiply(BigDecimal.valueOf(cantidadCalculada)) " + ci.getPrecio().multiply(ir.getCantidad()).multiply(BigDecimal.valueOf(cantidadCalculada)));
-
-
-               precioTotal = (ci.getPrecio().multiply(ir.getCantidad())).multiply(BigDecimal.valueOf(cantidadCalculada)).doubleValue();
-               
-//               System.out.print("\nprecioTotal " + precioTotal);
-
-                if(modelTable.length > 0 ){
-                                  
-                   modelTable[0].addRow(new Object[]{dto.getInsumo(), dto.getUnidadMedidad(), dto.getCantidad()});
-
-               }
-                    	
-            }
-        
-        BigDecimal bd = new BigDecimal(precioTotal);
-
-        return bd;
-    }
-         
-         
-	public void abrirSesion(){
-            
-            session = HibernateUtil.getSessionFactory().openSession();
-
-//            session.beginTransaction();
-=======
         TypedQuery query = session.getNamedQuery("Producto.findAll");
         productos = query.getResultList();
 
         session.getTransaction().commit();
         //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
-        session.close();
+//                session.close();
 
-        return productos;
+        return query.getResultList();
     }
 
     public List<Producto> obtenerProductosElaborados() {
-
-        session.beginTransaction();
-
         List<Producto> productos;
 
         TypedQuery query = session.getNamedQuery("Producto.findByTipo");
@@ -182,13 +59,29 @@ public class ControladorProductos {
 
         session.getTransaction().commit();
         //No cerrar la session mientras se piense utilizar mas metodos con query o generara una excepcion
+//                session.close();
+
+        return query.getResultList();
+    }
+
+    public void crearProducto(Producto producto) {
+
+        session.save(producto);
+
+    }
+
+    public void cerrarSesion() {
+
+//            session.getTransaction().commit();
         session.close();
 
-        return productos;
     }
 
     public Producto obtenerProductoByReceta(Recetas receta) {
 
+        if (!session.getTransaction().getStatus().equals("ACTIVA")) {
+            session = HibernateUtil.getSessionFactory().openSession();
+        }
         session.beginTransaction();
 
         Producto producto;
@@ -203,14 +96,6 @@ public class ControladorProductos {
         session.close();
 
         return producto;
-    }
-
-    public void crearProducto(Producto producto) {
-
-        session.beginTransaction();
-
-        session.save(producto);
-
     }
 
     public void borrarProducto(Producto producto) {
@@ -229,7 +114,74 @@ public class ControladorProductos {
         }
 
     }
->>>>>>> 63e072d370d50f62cdb8b7ff7d70146566ac779a
+
+    public BigDecimal calcularPrecioProduccion(Producto producto, double cantidad, DefaultTableModel... modelTable) {
+
+        this.cerrarSesion();
+        List<formAgregarInsumoProductoDTO> listaInsumosReceta = new ArrayList<formAgregarInsumoProductoDTO>();
+
+        ControladorReceta cr = new ControladorReceta();
+        Recetas r = new Recetas();
+        r = cr.obtenerRecetaPorId(producto.getIdReceta());
+
+        double cantidadReceta = r.getCantidad();
+        double cantidadCalculada = 0.0;
+        double precioTotal = 0;
+        double precio = 0.0;
+
+        ControladorComprasInsumos cpi = new ControladorComprasInsumos();
+
+        if (cantidadReceta > cantidad) {
+            cantidadCalculada = cantidad / cantidadReceta;
+        } else if (cantidadReceta < cantidad) {
+            cantidadCalculada = cantidad / cantidadReceta;
+
+        } else if (cantidadReceta == cantidad) {
+            cantidadCalculada = 1;
+        }
+
+//         List<InsumoRecetas> lir =  (List<InsumoRecetas>) r.getInsumoRecetasCollection();
+        cr.abrirSesion();
+
+        if (modelTable.length > 0) {
+            modelTable[0].setRowCount(0);
+
+        }
+
+        for (InsumoRecetas ir : r.getInsumoRecetasCollection()) {
+
+            formAgregarInsumoProductoDTO dto = new formAgregarInsumoProductoDTO();
+
+            dto.setCantidad(ir.getCantidad().doubleValue() * cantidadCalculada);
+            dto.setInsumo(ir.getIdInsumo());
+            dto.setUnidadMedidad(ir.getUnidadMedida());
+
+            listaInsumosReceta.add(dto);
+
+            ComprasInsumo ci = cpi.obtenerComprasdeUnInsumoUnico(ir.getIdInsumo());
+
+//               
+//               System.out.print("\nci.getPrecio() " + ci.getPrecio().toString());
+//               System.out.print("\nir.getCantidad() " + ir.getCantidad().toString());
+//               System.out.print("\n ci.getPrecio().multiply(ir.getCantidad()) " + ci.getPrecio().multiply(ir.getCantidad()).toString());
+//
+//               System.out.print("\ncantidadCalculada " + cantidadCalculada);
+//               System.out.print("\n ci.getPrecio().multiply(ir.getCantidad()).multiply(BigDecimal.valueOf(cantidadCalculada)) " + ci.getPrecio().multiply(ir.getCantidad()).multiply(BigDecimal.valueOf(cantidadCalculada)));
+            precioTotal = (ci.getPrecio().multiply(ir.getCantidad())).multiply(BigDecimal.valueOf(cantidadCalculada)).doubleValue();
+
+//               System.out.print("\nprecioTotal " + precioTotal);
+            if (modelTable.length > 0) {
+
+                modelTable[0].addRow(new Object[]{dto.getInsumo(), dto.getUnidadMedidad(), dto.getCantidad()});
+
+            }
+
+        }
+
+        BigDecimal bd = new BigDecimal(precioTotal);
+
+        return bd;
+    }
 
     public void abrirSesion() {
 
@@ -239,9 +191,13 @@ public class ControladorProductos {
 
     }
 
-    public void cerrarSesion() {
+    public void actualizarProducto(Producto producto) {
 
-        session.close();
+        session.beginTransaction();
+
+        session.update(producto);
+
+        session.getTransaction().commit();
 
     }
 
